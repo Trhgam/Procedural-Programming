8ef482ec970785c4eba55fe572ec85081a07185e
Trhgam <honggamdx2@gmail.com>
2025-11-08 19:22:45 +0700

    04-B1

---
+++ 04-B1/main.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+void process(int number1, int number2,int number3);
+int main()
+{   int number1,number2,number3;
+    printf("Enter a:");
+    scanf("%d", &number1);
+     printf("Enter b:");
+    scanf("%d", &number2);
+     printf("Enter c:");
+    scanf("%d", &number3);
+    process(number1,number2,number3);
+    return 0;
+}
+void process(int number1, int number2,int number3){
+      if( number1 != number2 && number1 != number3 && number2 != number3 ){
+            if(number1 > number2 && number1 > number3 ){
+                printf("%d is biggest number ", number1 );
+            }else if(number2 > number1 && number2 > number3 ){
+                printf("%d is biggest number ", number2 );
+            }else{
+                printf("%d is biggest number ", number3 );
+            }
+    }else if(number1 == number2 && number1 != number3 || number1 == number3 && number1 != number2 || number2 == number3 && number2 != number1){
+        if( number1 == number3 ){
+            if( number1 > number2 ){
+                printf("\n%d is biggest number ", number1);
+            }else{
+                printf("\n%d is biggest number ", number2);
+            }
+        }else if(number1 == number2 ){
+           if( number1 > number3 ){
+                printf("\n%d  is biggest number ", number1);
+           }else{
+                printf("\n%d  is biggest number ", number3);
+           }
+        }else{
+            if( number2 > number1 ){
+                printf("\n%d  is biggest number ", number2);
+            }else{
+                printf("\n%d  is biggest number ", number1);
+            }
+        }
+    }else{
+        printf("%d = %d = %d are biggest number ", number1, number2, number3);
+    }
+}
